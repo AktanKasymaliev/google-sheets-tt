ac71fa91b93a15aa60155b05e4264eaa287ef78c
AktanKasymaliev <dizzygame.dg@gmail.com>
2022-07-27 01:23:00 +0600

    some code comments added

--- app/logic.py
+++ app/logic.py
@@ -13,6 +13,12 @@ class Worker:
         self.data = self.sheet.read_sheet()['values']
 
     def parse_sheet_data(self) -> list:
+        """
+        It takes the data from the sheet, and then it divides it into a list of lists, 
+        where each list is a column of data.
+
+        :return: A list of lists.
+        """
         parsed_data = []
         for index in range(len(self.data[0])):
             parsed_data.append([
@@ -26,6 +32,9 @@ class Worker:
         return parsed_data
     
     def save_data_to_database(self):
+        """
+        It takes the data from the Google Sheet, and saves it to the database
+        """
         parsed_records = self.parse_sheet_data()
         try:
             for record in parsed_records:
@@ -48,5 +57,16 @@ class Worker:
         data: list, first_index: int, 
         second_index: int
         ) -> None:
+        """
+        > This function returns the data at the specified index
+        
+        :param data: list = The data that you want to divide
+        :type data: list
+        :param first_index: The index of the first list in the list of lists
+        :type first_index: int
+        :param second_index: The index of the data you want to return
+        :type second_index: int
+        :return: The data is being returned.
+        """
 
         return data[first_index][second_index]
