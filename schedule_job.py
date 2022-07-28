fbea997c35287c6dab2c7b615ced7eb529a1a924
AktanKasymaliev <dizzygame.dg@gmail.com>
2022-07-28 22:30:27 +0600

    schedule work added

---
+++ schedule_job.py
@@ -0,0 +1,25 @@
+import time
+from os import system
+from datetime import datetime
+
+import schedule
+
+def write_logs(path: str):
+    with open(path, 'a') as log:    
+        log.write(f'Data updated or saved at: {str(datetime.now())}\n')
+
+def job():
+    system('python main.py')
+
+def main():
+    print("\n*Schedule started. Every 5 minutes google-sheet reader bot works*")
+    job()
+    schedule.every(5).seconds.do(job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        write_logs('logs/logs.txt')
+
+if __name__ == "__main__":
+    main()
